064d1a15d5923e33b0f947f7531c21f2c8e5017c
hansg931 <hansg0509@cau.ac.kr>
2024-02-15 11:01:33 +0900

    Feat: 파이어베이스 적용

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:minto/src/app.dart';
 import 'package:minto/src/binding/init_bindings.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:minto/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
