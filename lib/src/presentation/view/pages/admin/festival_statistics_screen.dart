f2eccfe4653281dc0be2b356e114e2a7b07990c5
hansg931 <hansg0509@cau.ac.kr>
2024-05-02 22:15:49 +0900

    Fix: 관리자 페이지 UI 수정

--- lib/src/presentation/view/pages/admin/festival_statistics_screen.dart
+++ lib/src/presentation/view/pages/admin/festival_statistics_screen.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:get/get.dart';
+import 'package:minto/src/presentation/view/widgets/chart_widget.dart';
 
 class FestivalStatisticsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => Get.back(),
+        child: const Icon(Icons.arrow_back),
+      ),
       body: SafeArea(
         child: Center(
           child: ListView(
-            padding: EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(16.0),
             children: [
-              _buildPieChart(
+              ChartWidget.buildPieChart(
                 title: '남/여 비율',
                 data: [
                   ChartData('남성', 450),
                   ChartData('여성', 550),
                 ],
               ),
-              _buildPieChart(
+              ChartWidget.buildPieChart(
                 title: '연령대 분포',
                 data: [
                   ChartData('10대', 200),
@@ -27,7 +32,7 @@ class FestivalStatisticsScreen extends StatelessWidget {
                   ChartData('50대', 150),
                 ],
               ),
-              _buildLineChart(
+              ChartWidget.buildLineChart(
                 title: '일별 방문자 추이',
                 data: [
                   ChartData('4월 1일', 200),
@@ -39,14 +44,14 @@ class FestivalStatisticsScreen extends StatelessWidget {
                   ChartData('4월 7일', 800),
                 ],
               ),
-              _buildPieChart(
+              ChartWidget.buildPieChart(
                 title: '내/외국인 비율',
                 data: [
                   ChartData('내국인', 800),
                   ChartData('외국인', 700),
                 ],
               ),
-              _buildLineChart(
+              ChartWidget.buildLineChart(
                 title: '소비액 추이',
                 data: [
                   ChartData('4월 1일', 10000),
@@ -58,7 +63,7 @@ class FestivalStatisticsScreen extends StatelessWidget {
                   ChartData('4월 7일', 28000),
                 ],
               ),
-              _buildBarChart(
+              ChartWidget.buildBarChart(
                 title: '미션 완료 통계',
                 data: [
                   ChartData('미션1', 50),
@@ -73,80 +78,4 @@ class FestivalStatisticsScreen extends StatelessWidget {
       ),
     );
   }
-
-  Widget _buildPieChart(
-      {required String title, required List<ChartData> data}) {
-    return Column(
-      children: [
-        Text(title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        SizedBox(height: 10),
-        SfCircularChart(
-          legend: Legend(isVisible: true),
-          tooltipBehavior: TooltipBehavior(enable: true),
-          series: <CircularSeries>[
-            PieSeries<ChartData, String>(
-              dataSource: data,
-              xValueMapper: (ChartData data, _) => data.label,
-              yValueMapper: (ChartData data, _) => data.value,
-              dataLabelMapper: (ChartData data, _) => '${data.label}',
-              dataLabelSettings: DataLabelSettings(isVisible: true),
-            ),
-          ],
-        ),
-        SizedBox(height: 20),
-      ],
-    );
-  }
-
-  Widget _buildLineChart(
-      {required String title, required List<ChartData> data}) {
-    return Column(
-      children: [
-        Text(title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        SizedBox(height: 10),
-        SfCartesianChart(
-          primaryXAxis: CategoryAxis(),
-          series: <LineSeries<ChartData, String>>[
-            LineSeries<ChartData, String>(
-              dataSource: data,
-              xValueMapper: (ChartData data, _) => data.label,
-              yValueMapper: (ChartData data, _) => data.value,
-            ),
-          ],
-        ),
-        SizedBox(height: 20),
-      ],
-    );
-  }
-
-  Widget _buildBarChart(
-      {required String title, required List<ChartData> data}) {
-    return Column(
-      children: [
-        Text(title,
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        SizedBox(height: 10),
-        SfCartesianChart(
-          primaryXAxis: CategoryAxis(),
-          series: <BarSeries<ChartData, String>>[
-            BarSeries<ChartData, String>(
-              dataSource: data,
-              xValueMapper: (ChartData data, _) => data.label,
-              yValueMapper: (ChartData data, _) => data.value,
-            ),
-          ],
-        ),
-        SizedBox(height: 20),
-      ],
-    );
-  }
-}
-
-class ChartData {
-  final String label;
-  final double value;
-
-  ChartData(this.label, this.value);
 }
